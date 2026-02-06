8b95214879df9895e4fbf2d928df4bdf2bca98f7
matias <matiass@udel.edu>
2026-02-06 14:32:49 -0500

    Added a wonky bar

---
+++ src/Home.tsx
@@ -0,0 +1,13 @@
+export function Home(){
+    return(
+        <>
+        <h1>Matias Sayanes</h1>
+        <h3>About Me</h3>
+      <p>
+        I am a 21-year-old senior at the University of Delaware pursuing a Bachelor of Science in Computer Science with a concentration in AI and Robotics. I am eager to gain hands-on professional experience and apply my academic knowledge in real-world engineering and software development environments.
+        I have a strong interest in machine learning, automation, and building efficient, scalable systems. Looking ahead, I plan to pursue a Master of Science in Computer Science to further specialize in advanced computing and intelligent systems.
+        Outside of academics, I enjoy cooking and playing video games, interests that reflect my creativity, problem-solving mindset, and appreciation for well-designed systems.
+      </p>
+        </>
+    )
+}
---
+++ src/Skills.tsx
@@ -0,0 +1,10 @@
+export function Skills(){
+    return(
+        <>
+        <h3>Skills</h3>
+        <p>
+            I have strong programming skills in Python, Java, C++, R, TypeScript, SQL, and HTML/CSS, with hands-on experience using frameworks and libraries such as React, Node.js, PyTorch, and NumPy. I’m comfortable working in collaborative development environments using Git and following Agile and Scrum methodologies. In addition, I am fluent in Spanish, which supports effective communication in bilingual teams and diverse settings
+        </p>
+        </>
+    )
+}
